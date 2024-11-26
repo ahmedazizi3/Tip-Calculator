6a05b7229394a4fec1d2d28abd68ee84715fe4e3
DELL <aziziahmed027@gmail.com>
2024-11-26 11:59:23 +0100

    Final commit.

---
+++ app/src/main/java/azizi/ahmed/tip/MyTextField.kt
@@ -0,0 +1,67 @@
+package azizi.ahmed.tip
+
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    labelId: String,
+    enabled: Boolean,
+    isSingleLine: Boolean,
+    keyboardType: KeyboardType = KeyboardType.Number,
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    calculateTip: () -> Unit = {}
+) {
+
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = {
+            valueState.value = it
+            calculateTip
+        },
+        label = {
+            Text(
+                text = labelId,
+                color = Color(0xFFDE7CD1)
+            )
+        },
+        modifier = modifier,
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Star,
+                contentDescription = "Dollar",
+                tint = Color(0xFFDE7CD1)
+            )
+        },
+        singleLine = isSingleLine,
+        textStyle = TextStyle(
+            fontSize = 18.sp,
+            color = MaterialTheme.colorScheme.onBackground
+        ),
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            imeAction = imeAction,
+        ),
+        keyboardActions = keyboardActions
+    )
+}
